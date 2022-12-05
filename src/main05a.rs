0ad7a03e4912f00c13718040285d51b03e59d6dc
Konstantin Yegupov <kyegupov4@gmail.com>
2022-12-05 14:04:33 +0000

    AOC 2022

---
+++ src/main05a.rs
@@ -0,0 +1,48 @@
+use std::{error::Error, fs::read_to_string};
+
+use nom::{
+    bytes::complete::tag, character::complete::digit0, combinator::map, error, sequence::tuple,
+    IResult,
+};
+
+use itertools::Itertools;
+
+type Parser<'a, T> = fn(&'a str) -> IResult<&'a str, T>;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut stacks: Vec<Vec<char>> = vec![];
+    for (i, line) in read_to_string("input05.txt")?.lines().enumerate() {
+        if line.contains("[") {
+            let chars = line.chars().collect_vec();
+            for i in 0..((chars.len() + 3) / 4) {
+                while stacks.len() < i + 1 {
+                    stacks.push(vec![]);
+                }
+                let c = chars[i * 4 + 1];
+                if c != ' ' {
+                    stacks[i].push(c);
+                }
+            }
+        }
+        if line == "" {
+            for s in stacks.iter_mut() {
+                s.reverse();
+            }
+        }
+        if line.contains("move") {
+            let words = line.split(" ").collect_vec();
+            let qty: usize = words[1].parse()?;
+            let from: usize = words[3].parse()?;
+            let to: usize = words[5].parse()?;
+            for i in 0..qty {
+                let c = stacks[from - 1].pop().unwrap();
+                stacks[to - 1].push(c);
+            }
+        }
+    }
+
+    let s = stacks.iter().map(|s| s.last().unwrap()).join("");
+    println!("{s}");
+
+    Ok(())
+}
